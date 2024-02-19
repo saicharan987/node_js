8be268febdbaf7c5bc63d5d0b2cceba1600a5aaf
Saicharan S <shivanathunisaicharan@gmail.com>
2024-02-19 06:51:24 -0500

    this is practice

---
+++ demo/sample1.js
@@ -0,0 +1,14 @@
+//import js.util.Scanner;
+
+let a = 10;
+let b = 5;
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulo = a % b;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);      
+console.log(modulo);  
---
+++ demo/sample2.js
@@ -0,0 +1,8 @@
+ function add(x, y) {
+      var z = x + y;
+      return z;
+    }
+    let result = add(5, 8);
+    console.log(result);
+    let result1 = add(9, 10);
+    console.log(result1);
